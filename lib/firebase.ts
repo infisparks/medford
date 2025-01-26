ba3c01881edc1bb28674aefda72987697d471a6e
MK MUZAKKIR <muzakkir7977@gmail.com>
2025-01-26 17:23:07 -0500

    sdf

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,14 +4,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB6UXHfZdbn_jSJauHhbTwBvrFGsKnPeTw",
-  authDomain: "gautami-55545.firebaseapp.com",
-  databaseURL: "https://gautami-55545-default-rtdb.firebaseio.com",
-  projectId: "gautami-55545",
-  storageBucket: "gautami-55545.appspot.com",
-  messagingSenderId: "328668763634",
-  appId: "1:328668763634:web:5cd1be7de0e5e08aaa476b",
-  measurementId: "G-FZ93TQS67R"
+  apiKey: "AIzaSyCfyZ5k2RT_mkLR257MAoUMnnV24FnCFvE",
+  authDomain: "medford-491d4.firebaseapp.com",
+  projectId: "medford-491d4",
+  storageBucket: "medford-491d4.firebasestorage.app",
+  messagingSenderId: "595525614466",
+  appId: "1:595525614466:web:8f7280695e4f8c822ded9b",
+  measurementId: "G-Z7MX8Z8W7Q"
 };
 
 // Initialize Firebase
